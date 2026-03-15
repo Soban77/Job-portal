86ba77f075ab5cecb0ff869473709772436d5cee
Soban77 <sobanamjad87@gmail.com>
2026-03-15 07:08:29 +0500

    completed application.js in routes

--- routes/Application.js
+++ routes/Application.js
@@ -1,1 +1,37 @@
-c
+const express = require('express');
+const router = express.Router();
+const application = require('../models/application.js');
+
+router.get('/', async (req,res) => {
+
+  try {
+
+    const all = await application.find({});
+
+    return res.status(200).json(all);
+
+  } catch(err) {
+
+    return res.status(500).json({ error: err.message});
+
+  }
+
+});
+
+router.post('/', async (req,res) => {
+
+  try {
+
+    const us = new application(req.body);
+    await us.save();
+    return res.status(201).json({ message: 'Successful' });
+
+  } catch(err) {
+
+    return res.status(500).json({ error: err.message });
+
+  }
+
+});
+
+module.exports = router;
